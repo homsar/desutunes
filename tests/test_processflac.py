a0f01d89ed5bf089df6031c74f9454e988891e5c
h0m54r <h0m54r@twitter.com>
2018-03-20 00:05:40 +0000

    two more tests

---
+++ tests/test_processflac.py
@@ -0,0 +1,66 @@
+# Tests the processflac function from processfile.py
+
+from desutunes.processfile import processflac
+from datetime import datetime, timedelta
+import os
+import pathlib
+import pytest
+path = pathlib.Path(os.path.dirname(os.path.realpath(__file__)))
+
+
+class File_flac():
+    def __init__(self):
+        self.file = str(path / "test_audio.flac")
+        self.time_before_start = datetime.now()
+        self.result = processflac(self.file)
+        self.time_after_end = datetime.now()
+        assert len(self.result) == 1
+        self.metadata = self.result[0]
+
+
+@pytest.fixture
+def file_flac():
+    return File_flac()
+
+
+class Test_processflac:
+    def test_original_file_name(self, file_flac):
+        assert file_flac.metadata.OriginalFileName == file_flac.file
+
+    def test_new_file_name(self, file_flac):
+        assert str(file_flac.metadata.Filename)[:-22] == (
+            'Tachibana Kanade/Test FLAC file ('
+        )
+        assert str(file_flac.metadata.Filename)[-6:] == ').flac'
+
+    def test_title(self, file_flac):
+        assert file_flac.metadata.Tracktitle == 'Test FLAC file'
+
+    def test_album(self, file_flac):
+        assert file_flac.metadata.Album == 'Sounds of the desutunes'
+
+    def test_length(self, file_flac):
+        assert file_flac.metadata.Length == 2124
+
+    def test_anime(self, file_flac):
+        assert file_flac.metadata.Anime == 'Spam'
+        assert file_flac.metadata.Role == 'insert song'
+        assert file_flac.metadata.Rolequalifier == ''
+
+    def test_artist(self, file_flac):
+        assert file_flac.metadata.Artist == 'Tachibana Kanade'
+
+    def test_composer(self, file_flac):
+        assert file_flac.metadata.Composer == '🐱'
+
+    def test_label(self, file_flac):
+        assert file_flac.metadata.Label == 'h0m54r records'
+
+    def test_inmyriad(self, file_flac):
+        assert file_flac.metadata.InMyriad == 'NO'
+
+    def test_time(self, file_flac):
+        time_read = datetime.strptime(file_flac.metadata.Dateadded,
+                                      "%Y-%m-%d %H:%M")
+        assert file_flac.time_before_start < time_read + timedelta(minutes=1)
+        assert time_read < file_flac.time_after_end
---
+++ tests/test_processm4a.py
@@ -0,0 +1,66 @@
+# Tests the processm4a function from processfile.py
+
+from desutunes.processfile import processm4a
+from datetime import datetime, timedelta
+import os
+import pathlib
+import pytest
+path = pathlib.Path(os.path.dirname(os.path.realpath(__file__)))
+
+
+class File_m4a():
+    def __init__(self):
+        self.file = str(path / "test_audio.m4a")
+        self.time_before_start = datetime.now()
+        self.result = processm4a(self.file)
+        self.time_after_end = datetime.now()
+        assert len(self.result) == 1
+        self.metadata = self.result[0]
+
+
+@pytest.fixture
+def file_m4a():
+    return File_m4a()
+
+
+class Test_processid3_M4A:
+    def test_original_file_name(self, file_m4a):
+        assert file_m4a.metadata.OriginalFileName == file_m4a.file
+
+    def test_new_file_name(self, file_m4a):
+        assert str(file_m4a.metadata.Filename)[:-21] == (
+            'Tachibana Kanade/Test M4A file ('
+        )
+        assert str(file_m4a.metadata.Filename)[-5:] == ').m4a'
+
+    def test_title(self, file_m4a):
+        assert file_m4a.metadata.Tracktitle == 'Test M4A file'
+
+    def test_album(self, file_m4a):
+        assert file_m4a.metadata.Album == 'Sounds of the desutunes'
+
+    def test_length(self, file_m4a):
+        assert file_m4a.metadata.Length == 2148
+
+    def test_anime(self, file_m4a):
+        assert file_m4a.metadata.Anime == 'Spam'
+        assert file_m4a.metadata.Role == 'ED'
+        assert file_m4a.metadata.Rolequalifier == 'episode 3'
+
+    def test_artist(self, file_m4a):
+        assert file_m4a.metadata.Artist == 'Tachibana Kanade'
+
+    def test_composer(self, file_m4a):
+        assert file_m4a.metadata.Composer == '🐱'
+
+    def test_label(self, file_m4a):
+        assert file_m4a.metadata.Label == 'h0m54r records'
+
+    def test_inmyriad(self, file_m4a):
+        assert file_m4a.metadata.InMyriad == 'NO'
+
+    def test_time(self, file_m4a):
+        time_read = datetime.strptime(file_m4a.metadata.Dateadded,
+                                      "%Y-%m-%d %H:%M")
+        assert file_m4a.time_before_start < time_read + timedelta(minutes=1)
+        assert time_read < file_m4a.time_after_end
