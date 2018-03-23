03ce455d4bdba81993fc2e1fa3ef67f97255dee1
h0m54r <h0m54r@twitter.com>
2018-03-23 23:14:49 +0000

    test title splitting

---
+++ tests/test_split_id3_title.py
@@ -0,0 +1,27 @@
+# Test the split_id3_title function from processfile.py
+
+from desutunes.processfile import split_id3_title
+
+
+def test_split_id3_title_simple():
+    assert (split_id3_title("Lion (Macross Frontier OP2)") ==
+            ("Lion", "Macross Frontier OP2"))
+
+
+def test_split_id3_title_extra_bracket():
+    assert (split_id3_title("CHANGE!!!! (M@STER VERSION) "
+                            "(The iDOLM@STER OP2)") ==
+            ("CHANGE!!!! (M@STER VERSION)",
+             "The iDOLM@STER OP2"))
+
+
+def test_split_id3_title_double_bracket():
+    assert (split_id3_title("CHANGE!!!! ((M@STER VERSION)) "
+                            "(The iDOLM@STER OP2)") ==
+            ("CHANGE!!!! ((M@STER VERSION))",
+             "The iDOLM@STER OP2"))
+
+
+def test_missing_detail():
+    assert (split_id3_title("Ai oboeteimasu ka") ==
+            ("Ai oboeteimasu ka", None))
